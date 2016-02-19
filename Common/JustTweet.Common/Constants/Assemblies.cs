051f9ce3cba569ac08858640a355436d672e6c1b
Novkirishki <novkirishki94@gmail.com>
2016-02-19 20:04:34 +0200

    Removed unnecessary files and projects

--- Common/JustTweet.Common/Constants/Assemblies.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace JustTweet.Common.Constants
-{
-    public class Assemblies
-    {
-        public const string WebApi = "JustTweet.Web.Api";
-    }
-}
--- Web/JustTweet.Web.Wcf/Models/UserDetailsResponseModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace JustTweet.Web.Wcf.Models
-{
-    public class UserDetailsResponseModel
-    {
-        public string UserName { get; set; }
-
-        public double Rating { get; set; }
-    }
-}
